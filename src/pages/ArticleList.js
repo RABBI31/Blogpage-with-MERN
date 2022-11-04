e3004e5a2299ab22553a0016806e4e13e861be1d
Md.Ali Rabbi <56753742+RABBI31@users.noreply.github.com>
2022-11-04 17:43:24 +0600

    Pages added

---
+++ src/pages/ArticleList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticleList = () => {
+  return (
+    <h1 className='sm:text-4xl text-2xl font-bold text-gray-900 mb-10'>
+        Articles
+    </h1>
+  )
+}
+
+export default ArticleList
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold text-gray-900 mb-10'>Hello, Welcome to my page</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+        <b>Hello, Welcome to Blog ! </b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem explicabo officia omnis sint aliquid in distinctio esse accusantium, culpa odit nemo error, nam exercitationem nobis quibusdam repellat quisquam veritatis quia dolor illum, provident tempora voluptas? Explicabo repellat vitae eligendi. Minima quidem iste alias fuga saepe nemo hic odit odio similique.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem explicabo officia omnis sint aliquid in distinctio esse accusantium, culpa odit nemo error, nam exercitationem nobis quibusdam repellat quisquam veritatis quia dolor illum, provident tempora voluptas? Explicabo repellat vitae eligendi. Minima quidem iste alias fuga saepe nemo hic odit odio similique.
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem explicabo officia omnis sint aliquid in distinctio esse accusantium, culpa odit nemo error, nam exercitationem nobis quibusdam repellat quisquam veritatis quia dolor illum, provident tempora voluptas? Explicabo repellat vitae eligendi. Minima quidem iste alias fuga saepe nemo hic odit odio similique.
+        </p>
+    </div>
+  )
+}
+
+export default Home
