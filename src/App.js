37ae26d8cc260088a696658b2b153038ef135d5a
Md.Ali Rabbi <56753742+RABBI31@users.noreply.github.com>
2022-11-05 12:26:14 +0600

    Article setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import {  Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Routes , Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import ArticleList from './pages/ArticleList';
 import Article from './pages/Article'
 import './App.css';
+import Navbar from './componenets/Navbar';
+
 
 function App() {
   return (
+    <BrowserRouter >
+    <Navbar />
     <div className="max-w-screen-md mx-auto pt-20">
       <Routes>
-        <Route path = "/" element={<Home />} />
-        <Route path = "/about" element={<About />} />
-        <Route path = "/article-list" element={<ArticleList />} />
-        <Route path = "/article" element={<Article />} />
+        <Route exact path = "/" element={<Home />} />
+        <Route exact path = "/about" element={<About />} />
+        <Route exact path = "/article-list" element={<ArticleList />} />
+        <Route exact path = "/article/:id" element={<Article />} />
       </Routes>
+    
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import articles from './ArticleContent'
 
 const Article = () => {
+  const {id} = useParams()
+  const article = articles.find((ar) => ar.id === Number(id));
+  if(!article) return <h1 className='sm:text-4xl text-2xl font-bold text-gray-900 mb-10'>Article does not existed</h1>
   return (
     <>
     <h1 className='sm:text-4xl text-2xl font-bold text-gray-900 mb-10'>
-        Articles
+        {article.title}
     </h1>
+    {article.content.map((paragraph, index)=>{
+      return(
+          <p className='mx-auto leading-relaxed text-base mb-4'>{paragraph}</p>   
+      )
+      
+    })}
     </>
   )
 }
